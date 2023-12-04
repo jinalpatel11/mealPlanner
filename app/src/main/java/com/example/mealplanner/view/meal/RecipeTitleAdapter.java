31cbf39097faa704df037db3f51eb4b47504b348
jinalpatel1112 <jpatel6625@conestogac.on.ca>
2023-12-04 12:59:21 -0500

    view all page design is create

--- app/src/main/java/com/example/mealplanner/view/meal/RecipeTitleAdapter.java
+++ app/src/main/java/com/example/mealplanner/view/meal/RecipeTitleAdapter.java
@@ -1,38 +1,36 @@
 package com.example.mealplanner.view.meal;
-// RecipesAdapter.java
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mealplanner.R;
-import com.example.mealplanner.model.RecipeItem;
 
 import java.util.List;
 
-public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipeViewHolder> {
+public class RecipeTitleAdapter extends RecyclerView.Adapter<RecipeTitleAdapter.RecipeViewHolder> {
 
-    private List<RecipeItem> recipeItems;
+    private List<String> recipeItems;
 
-    public RecipesAdapter(List<RecipeItem> recipeItems) {
+    public RecipeTitleAdapter(List<String> recipeItems) {
         this.recipeItems = recipeItems;
     }
 
     @NonNull
     @Override
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recipe_title_image, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recipe_title, parent, false);
         return new RecipeViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
-        RecipeItem recipeItem = recipeItems.get(position);
-        holder.recipeItemImageView.setImageResource(recipeItem.getImageResource());
-        holder.recipeItemTextView.setText(recipeItem.getText());
+        String recipeItem = recipeItems.get(position);
+        holder.recipeItemTextView.setText(recipeItem);
 
         // Set click listener or other properties as needed for the item
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -49,12 +47,11 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipeVi
     }
 
     static class RecipeViewHolder extends RecyclerView.ViewHolder {
-        ImageView recipeItemImageView;
+
         TextView recipeItemTextView;
 
         RecipeViewHolder(@NonNull View itemView) {
             super(itemView);
-            recipeItemImageView = itemView.findViewById(R.id.recipeItemImageView);
             recipeItemTextView = itemView.findViewById(R.id.recipeItemTextView);
         }
     }
--- app/src/main/java/com/example/mealplanner/view/meal/ViewAllRecipesActivity.java
+++ app/src/main/java/com/example/mealplanner/view/meal/ViewAllRecipesActivity.java
@@ -5,8 +5,14 @@ package com.example.mealplanner.view.meal;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mealplanner.R;
+import com.example.mealplanner.model.RecipeItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewAllRecipesActivity extends AppCompatActivity {
 
@@ -15,6 +21,31 @@ public class ViewAllRecipesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all_recipes);
 
-        // Add any additional setup or logic specific to this activity
+        // Recipes RecyclerView
+        RecyclerView recipesRecyclerView = findViewById(R.id.recipesTitleRecyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recipesRecyclerView.setLayoutManager(layoutManager);
+
+        // Create a list of static recipe titles
+        List<String> recipeTitles = new ArrayList<>();
+        recipeTitles.add("All Premium Recipes");
+        recipeTitles.add("Quick & Easy Recipes");
+        recipeTitles.add("Main Courses");
+        recipeTitles.add("Breakfasts");
+        recipeTitles.add("Lunch");
+        recipeTitles.add("Beverages");
+        recipeTitles.add("Salads");
+        recipeTitles.add("Snacks");
+        recipeTitles.add("Desserts");
+        recipeTitles.add("Sides");
+        recipeTitles.add("Vegetarian");
+        recipeTitles.add("Paleo");
+        recipeTitles.add("Gluten Free");
+        recipeTitles.add("Fasting Friendly");
+
+
+        // Set up the adapter for the RecyclerView
+        RecipeTitleAdapter recipesAdapter = new RecipeTitleAdapter(recipeTitles);
+        recipesRecyclerView.setAdapter(recipesAdapter);
     }
 }
