f27e4a20f847aeae70036af5fddc5528cb3dbc5b
jinalpatel1112 <jpatel6625@conestogac.on.ca>
2023-12-06 21:00:44 -0500

    validation is created for registration page

--- app/src/main/java/com/example/mealplanner/view/auth/RegistrationActivity.java
+++ app/src/main/java/com/example/mealplanner/view/auth/RegistrationActivity.java
@@ -3,13 +3,22 @@ package com.example.mealplanner.view.auth;
 // RegistrationActivity.java
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mealplanner.R;
 import com.example.mealplanner.controller.UserController;
 import com.example.mealplanner.databinding.ActivityRegistrationBinding;
 import com.example.mealplanner.repositories.UserRepository;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
 
 public class RegistrationActivity extends AppCompatActivity {
     private UserController userController;
@@ -30,18 +39,26 @@ public class RegistrationActivity extends AppCompatActivity {
         binding.buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = binding.editTextFirstName.getText().toString();
-                String password = binding.editTextPassword.getText().toString();
 
-                if (userController.register(username, password)) {
-                    // Successful registration
-                    Toast.makeText(RegistrationActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
-                    // Redirect to LoginActivity
-                    redirectToLogin();
-                } else {
-                    // Failed registration (username already exists)
-                    Toast.makeText(RegistrationActivity.this, "Email already exists", Toast.LENGTH_SHORT).show();
+                if(validateFields())
+                {
+                    String username = binding.editTextFirstName.getText().toString();
+                    String password = binding.editTextPassword.getText().toString();
+
+                    if (userController.register(username, password)) {
+                        // Successful registration
+                        Toast.makeText(RegistrationActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                        // Redirect to LoginActivity
+                        redirectToLogin();
+                    } else {
+                        // Failed registration (username already exists)
+                        Toast.makeText(RegistrationActivity.this, "Email already exists", Toast.LENGTH_SHORT).show();
+                    }
+                }else
+                {
+
                 }
+
             }
         });
 
@@ -52,5 +69,52 @@ public class RegistrationActivity extends AppCompatActivity {
         startActivity(loginIntent);
         finish(); // Finish the RegistrationActivity to prevent going back
     }
+
+
+
+    // Validate user input fields
+    private boolean validateFields() {
+        boolean validationValue = true;
+        String name = Objects.requireNonNull(binding.editTextFirstName.getText()).toString().trim();
+        String email = Objects.requireNonNull(binding.editTextEmail.getText()).toString().trim();
+        String password = Objects.requireNonNull(binding.editTextPassword.getText()).toString().trim();
+
+        if (name.isEmpty()) {
+            binding.editTextFirstName.setError(getString(R.string.name_required));
+            validationValue = false;
+        } else {
+            binding.editTextFirstName.setError(null);
+        }
+
+        if (email.isEmpty()) {
+            binding.editTextEmail.setError(getString(R.string.email_required));
+            validationValue = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.editTextEmail.setError(getString(R.string.invalid_email));
+            validationValue = false;
+        } else {
+            binding.editTextEmail.setError(null);
+        }
+
+        // Validate password
+        if (password.isEmpty()) {
+            binding.editTextPassword.setError(getString(R.string.password_required));
+            validationValue = false;
+        } else if (!isPasswordValid(password)) {
+            binding.editTextPassword.setError(getString(R.string.invalid_password));
+            validationValue = false;
+        } else {
+            binding.editTextPassword.setError(null);
+        }
+
+        return validationValue;
+    }
+
+    private boolean isPasswordValid(String password) {
+        // Use a regular expression to check if the password meets the criteria
+        String passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
+        return password.matches(passwordPattern);
+    }
+
 }
 
